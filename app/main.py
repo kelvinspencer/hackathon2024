a94a4225339bbf2bfba09bfe861471aab58bd13c
lempira <lempira@gmail.com>
2024-02-24 15:07:41 -0500

    Added endpoint

--- app/main.py
+++ app/main.py
@@ -1,44 +1,24 @@
-from typing import Union
 from fastapi import FastAPI
-from fastapi import FastAPI, Body
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
-import pandas as pd
 
 app = FastAPI()
-LOCAL_DATABASE = "./app/data_base.csv"
 
+# Configure CORS settings
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-def init_bd():
-    dictionary = {"name": ["Computer"], "description": ["Macbook"], "price": [1000], "tax": [5]}
-    base_data = pd.DataFrame.from_dict(dictionary)
-    return base_data
+class Query(BaseModel):
+    query: str
 
-
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.get("/make_add")
-def make_add():
-    return {"add": 4 + 6}
-
-@app.post("/items/", status_code=201)
-async def create_item(item: Item = Body(...)):
-    base_data = init_bd()
-    base_data = base_data._append(item.__dict__, ignore_index=True)
-    base_data.to_csv(LOCAL_DATABASE, index_label="id")
-    return item
+@app.post("/docs/query")
+async def docs_query(query: Query):
+    print(query.query)
+    query_str = query.query
+    return {"results": query_str}
